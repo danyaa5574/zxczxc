6de795ccb68a26752789952dbc2c18be4b18da2e
Мишкотис Даниил Николаевич <pr11mishkotis@oat.local>
2023-05-23 17:38:35 +0600

    EigthDay

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -11,4 +11,27 @@ public class MainActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
     }
+    //Initializing thetextView
+    TextView textView;
+    textView = findViewById(R.id.hint);
+
+        new CountDownTimer(60000, 1000)
+    {
+        @Override
+        public void onTick(long millisUntilFinished) {
+        //Used for formatting digit to be in 2 digits only
+        NumberFormat f = new DecimalFormat("00");
+
+        long sec = (millisUntilFinished/ 1000) % 60;
+        textView.setText("Отправить код повторно можно будет через " + f.format(sec) + " секунд");
+    }
+        //When the ask is over it will print 00:00:00 there
+        public void onFinish() {
+        textView.setText("Отправить код повторно можно будет через 0 секунд");
+    }
+    }.start();
+        }
 }
+}
+
+
